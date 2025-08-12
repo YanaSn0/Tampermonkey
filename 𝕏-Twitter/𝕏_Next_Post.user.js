918e6351b310a1a068036aef43e48baa59c70c38
Yana_Sn0 <yanasnowbals@proton.me>
2025-08-12 00:41:19 -0800

    Update 𝕏_Next_Post.user.js

--- 𝕏-Twitter/𝕏_Next_Post.user.js
+++ 𝕏-Twitter/𝕏_Next_Post.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      6.0
 // @description  Highlight post(s) (persistent green border) on reply button click, keep highlight after submit until new reply exceeds highlight count,
-// @description   scroll to next post top with adjustable delay on X.com home page
+// @description   scroll to next post top with adjustable delay on X.com home page.
 // @author       YanaSn0w1
 // @match        https://x.com/home
 // @run-at       document-idle

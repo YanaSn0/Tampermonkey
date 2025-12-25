a4a879c6f96bb88360d96ca174bd196a859d2b09
YanaSn0w1 <207849788+YanaSn0w1@users.noreply.github.com>
2025-12-25 14:44:58 -0800

    Update user script description for clarity

--- 𝕏-Twitter/𝕏-mutual.user.js
+++ 𝕏-Twitter/𝕏-mutual.user.js
@@ -2,7 +2,7 @@
 // @name         𝕏-mutual
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Scrape to skip "bots" while fb normal accounts.
+// @description  Follow back all who follow.
 // @author       YanaSn0w1
 // @match        https://x.com/*followers
 // @grant        none

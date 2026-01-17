04c7b1a6cdb3a73053f15f0e78ea47ec0fcb1745
YanaSn0w1 <207849788+YanaSn0w1@users.noreply.github.com>
2026-01-16 18:03:48 -0800

    Update 𝕏-Mutual-Manager-Pro-Plus.user.js

--- 𝕏-Twitter/𝕏-Mutual-Manager-Pro-Plus.user.js
+++ 𝕏-Twitter/𝕏-Mutual-Manager-Pro-Plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         𝕏-Mutual-Manager-Pro-Plus
 // @namespace    http://tampermonkey.net/
-// @version      1.1.2
+// @version      1.1.3
 // @author       YanaHeat
 // @match        https://x.com/*
 // @grant        none

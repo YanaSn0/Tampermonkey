5319cc2bcd8230cbafb2f92762da0fca468399f9
YanaSn0w1 <207849788+YanaSn0w1@users.noreply.github.com>
2026-01-27 09:11:02 -0800

    Bump version to 1.0.2

--- 𝕏-Twitter/𝕏-Mutual-Manager-Pro.user.js
+++ 𝕏-Twitter/𝕏-Mutual-Manager-Pro.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         𝕏-Mutual-Manager-Pro
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @author       YanaHeat
 // @match        https://x.com/*follow*
 // @grant        none
